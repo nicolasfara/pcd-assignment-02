8481b327a6c673c1ada2fe24ea5d9120c43da5ef
Martina <martina.cavallucci96@gmail.com>
2020-04-20 23:57:46 +0200

    Base wiki query implementation

--- src/main/kotlin/it/unibo/pcd/EntryPoint.kt
+++ src/main/kotlin/it/unibo/pcd/EntryPoint.kt
@@ -1,5 +1,27 @@
 package it.unibo.pcd
 
+import java.net.HttpURLConnection
+import java.net.URL
+
 fun main(args: Array<String>) {
-    println("Hello world")
+    // an user example url passed
+    val userUrl = "https://it.wikipedia.org/wiki/Linguaggio_di_programmazione"
+    sendGet(userUrl)
+}
+
+fun sendGet(userUrl: String) {
+    val topic = userUrl.substringAfter("wiki/")
+    val url = URL("https://it.wikipedia.org/w/api.php?action=parse&page=$topic&format=json&section=0&prop=links")
+
+    with(url.openConnection() as HttpURLConnection) {
+        requestMethod = "GET"  // optional default is GET
+
+        println("\nSent 'GET' request to URL : $url; Response Code : $responseCode")
+
+        inputStream.bufferedReader().use {
+            it.lines().forEach { line ->
+                println(line)
+            }
+        }
+    }
 }
