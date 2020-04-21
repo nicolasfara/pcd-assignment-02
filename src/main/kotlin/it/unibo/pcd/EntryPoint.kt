efdf99415941ec7dad0a5f2e76cf3cd27606c485
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2020-04-21 23:22:07 +0200

    Add graph

--- src/main/kotlin/it/unibo/pcd/EntryPoint.kt
+++ src/main/kotlin/it/unibo/pcd/EntryPoint.kt
@@ -1,8 +1,30 @@
 package it.unibo.pcd
 
+import kotlin.IllegalArgumentException
+
 fun main(args: Array<String>) {
     // an user example url passed
     val userUrl = "https://it.wikipedia.org/wiki/Linguaggio_di_programmazione"
     val nodeExample = Node()
     print(nodeExample.fromUrl(userUrl))
+
+    val graph = Graph<String>()
+
+    try {
+        graph.addUniqueEdge("N1", "N2")
+        graph.addUniqueEdge("N2", "N3")
+        graph.addUniqueEdge("N3", "N4")
+        graph.addUniqueEdge("N5", "N2")
+    } catch (ex: IllegalArgumentException) {
+        println(ex.message)
+    }
+
+    println(graph.toString())
+    println(graph.isVertexPresent("N5"))
 }
+
+/*
+    N1 -> a1, a3
+    N2 -> a1, a2
+    N3 -> a2, a3
+ */
