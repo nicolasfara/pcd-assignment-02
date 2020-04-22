3231d57d5604f32c1e9ed876ec29c846b4c45b25
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2020-04-22 16:37:41 +0200

    Override toString for a better comprehension

--- src/main/kotlin/it/unibo/pcd/data/WikiPage.kt
+++ src/main/kotlin/it/unibo/pcd/data/WikiPage.kt
@@ -1,3 +1,7 @@
 package it.unibo.pcd.data
 
-data class WikiPage(val baseURL: String, val description: String, val links: MutableSet<String> = mutableSetOf())
+data class WikiPage(val baseURL: String, val description: String, val links: MutableSet<String> = mutableSetOf()) {
+    override fun toString(): String {
+        return "WikiPage(url: $baseURL, desc: $description, links: ${links.size})"
+    }
+}
