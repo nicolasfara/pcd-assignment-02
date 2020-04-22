6460d425fdcd116fd57fe3fbc00e04933e6d15be
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2020-04-22 16:38:16 +0200

    Improvment on how retreive content on page

--- src/main/kotlin/it/unibo/pcd/network/WikiCrawler.kt
+++ src/main/kotlin/it/unibo/pcd/network/WikiCrawler.kt
@@ -1,22 +1,22 @@
 package it.unibo.pcd.network
 
-import com.google.gson.Gson
-import com.google.gson.JsonObject
-import java.net.HttpURLConnection
-import java.net.URL
-import java.net.URLDecoder
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
 
 class WikiCrawler {
 
-    private val baseWikiURL = "https://it.wikipedia.org/wiki/"
+    private val baseWikiUrl = "https://it.wikipedia.org/wiki/"
 
     fun getLinksFromAbstract(pageURL: String) : Collection<String> {
-        return parseUrl(sendGet(pageURL))
+        val doc: Document = Jsoup.connect(normalizeUrlForApi(pageURL)).get()
+        val newsHeadlines: Elements = doc.select("section:first-child p > a:not([href*=#])")
+        return newsHeadlines.map { baseWikiUrl + it.attr("title").replace(" ", "_") }
     }
 
     fun getDescriptionFromPage(pageURL: String): String {
-        val topic = pageURL.substringAfter("wiki/")
-        val urlGET = URL("https://it.wikipedia.org/w/api.php?action=parse&page=$topic&format=json&prop=description")
+        /*val topic = pageURL.substringAfter("wiki/")
+        val urlGET = URL("https://en.wikipedia.org/w/api.php?action=parse&page=$topic&format=json&prop=description")
         val sBuilder = StringBuilder()
         with(urlGET.openConnection() as HttpURLConnection) {
             requestMethod = "GET"  // optional default is GET
@@ -27,32 +27,12 @@ class WikiCrawler {
                 }
             }
         }
-        return sBuilder.toString()
+        return sBuilder.toString()*/
+        return "empty"
     }
 
-    private fun sendGet(request: String): String {
-        val topic = request.substringAfter("wiki/")
-        val urlGET = URL(URLDecoder.decode("https://it.wikipedia.org/w/api.php?action=parse&page=$topic&format=json&section=0&prop=links", "UTF-8"))
-        val sBuilder = StringBuilder()
-        with(urlGET.openConnection() as HttpURLConnection) {
-            requestMethod = "GET"  // optional default is GET
-            //println("\nSent 'GET' request to URL : $urlGET; Response Code : $responseCode")
-            inputStream.bufferedReader().use {
-                it.lines().forEach { line ->
-                    sBuilder.append(line);
-                }
-            }
-        }
-        return sBuilder.toString()
-    }
-
-    private fun parseUrl(url: String): Collection<String> {
-        val firstLevel = Gson()
-            .fromJson(url, JsonObject::class.java)
-            .get("parse")
-            .asJsonObject
-
-        return firstLevel.get("links").asJsonArray.filter { it.asJsonObject.get("ns").asInt == 0 }
-            .map { baseWikiURL + it.asJsonObject.get("*").asString.replace(" " , "_") }
+    private fun normalizeUrlForApi(url: String): String {
+        val baseUrl = "https://it.wikipedia.org/api/rest_v1/page/html/"
+        return baseUrl + url.substringAfter("wiki/")
     }
 }
