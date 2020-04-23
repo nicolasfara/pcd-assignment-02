f4d26f9d95ae94b63fe3ebfd4844eedb276a0769
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2020-04-24 00:03:02 +0200

    Some fix

--- src/main/kotlin/it/unibo/pcd/forkjoin/LinkSearchAction.kt
+++ src/main/kotlin/it/unibo/pcd/forkjoin/LinkSearchAction.kt
@@ -4,7 +4,6 @@ import it.unibo.pcd.data.WikiPage
 import it.unibo.pcd.network.WikiCrawler
 import org.jgrapht.graph.DefaultEdge
 import org.jgrapht.graph.SimpleDirectedGraph
-import java.util.*
 import java.util.concurrent.RecursiveAction
 
 class LinkSearchAction(private val graph: SimpleDirectedGraph<WikiPage, DefaultEdge>, val depth: Int = 5, val startURL: String): RecursiveAction() {
@@ -20,15 +19,16 @@ class LinkSearchAction(private val graph: SimpleDirectedGraph<WikiPage, DefaultE
     private fun createSubAction() {
         var currentVertex = graph.vertexSet().find { it.baseURL == startURL }
 
-        // If no vertex are present in the graph, we create the entry point
-        if (currentVertex == null)
-            currentVertex = WikiPage(startURL, crawler.getDescriptionFromPage(startURL), entryNode = true)
+        if (crawler.getDescriptionFromPage(startURL) != "Url not found") {
+            // If no vertex are present in the graph, we create the entry point
+            if (currentVertex == null) {
+                currentVertex = WikiPage(startURL, crawler.getDescriptionFromPage(startURL), entryNode = true)
+            }
 
-        currentVertex.links.addAll(crawler.getLinksFromAbstract(startURL)) // Create all links inside the page
-        graph.addVertex(currentVertex) // Add the page (vertex) to the graph
+            currentVertex.links.addAll(crawler.getLinksFromAbstract(startURL)) // Create all links inside the page
+            graph.addVertex(currentVertex) // Add the page (vertex) to the graph
 
-        currentVertex.links.forEach {
-            if (it != "URL not found") {
+            currentVertex.links.forEach {
                 val linkVertex = WikiPage(it, crawler.getDescriptionFromPage(it), mutableSetOf())
                 // If the page is already present will not be add
                 if (!graph.vertexSet().map { e -> e.baseURL }.contains(linkVertex.baseURL)) {
@@ -36,15 +36,12 @@ class LinkSearchAction(private val graph: SimpleDirectedGraph<WikiPage, DefaultE
                     graph.addEdge(currentVertex, linkVertex)
                 }
             }
-        }
 
-        val l = Collections.synchronizedList(mutableListOf<LinkSearchAction>())
-
-        currentVertex.links.forEach {
-            val lsa = LinkSearchAction(graph, depth-1, it)
-            l.add(lsa)
-            lsa.fork()
-            lsa.join()
+            currentVertex.links.forEach {
+                val lsa = LinkSearchAction(graph, depth - 1, it)
+                lsa.fork()
+                lsa.join()
+            }
         }
     }
 }
