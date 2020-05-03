c96b4e67485b7434c50d3f86d6db2216bc28dc3e
Martina <martina.cavallucci96@gmail.com>
2020-05-03 17:17:30 +0200

    add customListItem

--- src/main/kotlin/it/unibo/pcd/MainView.kt
+++ src/main/kotlin/it/unibo/pcd/MainView.kt
@@ -4,47 +4,38 @@ import it.unibo.pcd.contract.Contract
 import it.unibo.pcd.model.WikiPage
 import it.unibo.pcd.presenter.CrawlerPresenter
 import it.unibo.pcd.presenter.SearchStrategy
+import it.unibo.pcd.view.LinkListViewCell
 import javafx.application.Platform
 import javafx.collections.FXCollections
-import javafx.scene.control.Button
-import javafx.scene.control.ComboBox
-import javafx.scene.control.ProgressIndicator
-import javafx.scene.control.TextField
-import javafx.scene.control.ListView
+import javafx.scene.control.*
 import javafx.scene.layout.BorderPane
-import javafx.scene.layout.StackPane
-import tornadofx.View
 import tornadofx.App
+import tornadofx.View
 import tornadofx.launch
-
-import kotlin.collections.HashSet
 
 class MainView : View("Wiki Link Search "), Contract.View {
     override val root: BorderPane by fxml("/MainView.fxml")
-    private val graphPane: StackPane by fxid()
     private val wikiUrl: TextField by fxid()
     private val depth: TextField by fxid()
     private val progress: ProgressIndicator by fxid()
     private val searchBtn: Button by fxid()
     private val combo: ComboBox<SearchStrategy> by fxid()
-    private val resSet = HashSet<String>()
-    private val itemsList = FXCollections.observableArrayList<String>()
-    private val listView = ListView(itemsList)
-
+    private val itemsList = FXCollections.observableArrayList<WikiPage>()
+    private val listView: ListView<WikiPage> by fxid()
     private val presenter: CrawlerPresenter
 
     init {
+        listView.setCellFactory { LinkListViewCell() }
+        listView.items = itemsList
         combo.items.addAll(SearchStrategy.values())
         presenter = CrawlerPresenter()
         presenter.attachView(this)
-        graphPane.children.add(listView)
     }
 
     override fun displaySearchResult(vertex: Set<WikiPage>) {
         Platform.runLater {
-            resSet.addAll(vertex.map { it.baseURL })
             itemsList.clear()
-            itemsList.addAll(resSet)
+            itemsList.addAll(vertex)
         }
     }
 
---
+++ src/main/kotlin/it/unibo/pcd/view/LinkListViewCell.kt
@@ -0,0 +1,38 @@
+package it.unibo.pcd.view
+
+import it.unibo.pcd.model.WikiPage
+import javafx.scene.control.Label
+import javafx.scene.control.ListCell
+import javafx.scene.layout.GridPane
+
+class LinkListViewCell : ListCell<WikiPage>() {
+
+    private val baseUrl = Label()
+    private val description = Label()
+    private val links = Label()
+    private var col = 0
+    private var firstRow = 0
+    private var secondRow = 1
+    private var thirdRow = 2
+    private var takeCharDescription = 100
+    private val linkSet = mutableSetOf<String>()
+
+    override fun updateItem(wikiPage: WikiPage?, empty: Boolean) {
+        super.updateItem(wikiPage, empty)
+        if (empty || wikiPage == null) {
+            text = null
+            graphic = null
+        } else {
+            val grid = GridPane()
+            baseUrl.text = wikiPage.baseURL.substringAfter("wiki/")
+            description.text = wikiPage.description.take(takeCharDescription)
+            wikiPage.links.forEach { linkSet.add(it.substringAfter("wiki/")) }
+            links.text = linkSet.toString()
+
+            grid.add(baseUrl, col, firstRow)
+            grid.add(description, col, secondRow)
+            grid.add(links, col, thirdRow)
+            graphic = grid
+        }
+    }
+}
